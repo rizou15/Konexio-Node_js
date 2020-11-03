3fadabf292f6bccf396f18f1ba8a20bef41b27f8
rizou15 <rizou1504@gmail.com>
2020-11-03 15:47:19 +0100

    exercice authors 1 à 4

--- express_basics/index.js
+++ express_basics/index.js
@@ -8,36 +8,75 @@ const app = express();
 // exercice 0
 
 app.get('/', (req, res) => {
-    //console.log(req);
-  //res.send('Authors API');
+  console.log(req);
+  res.send('Authors API');
 });
 
 
 // exercice 1
 const authors = ['Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US', 'Oscar Wilde, UK'];
 
-app.get('/authors/:id/', (req, res,next) => {
-  
-  res.send(authors[req.params.id -1]);
+app.get('/authors/:id/', (req, res, next) => {
+
+  res.send(authors[req.params.id - 1]);
 
 });
 
 // exercice 2
 
-const books= ['Beowulf','Hamlet, Othello, Romeo and Juliet, MacBeth','Oliver Twist, A Christmas Carol','The Picture of Dorian Gray, The Importance of Being Earnest'];
+const books = ['Beowulf', 'Hamlet, Othello, Romeo and Juliet, MacBeth', 'Oliver Twist, A Christmas Carol', 'The Picture of Dorian Gray, The Importance of Being Earnest'];
 
-app.get('/authors/:id/books/', (req, res,next) => {
-  
-    res.send(books[req.params.id -1]);
+app.get('/authors/:id/books/', (req, res, next) => {
+  const id = req.params.id;
 
+  if (id > authors.length) {
+
+    res.send(`the author with ${id} does not exist`);
+
+  } else {
+
+
+    res.send(books[id - 1]);
+  }
 });
 
-// Run server
+
+// exercice 3
+
+app.get('/cars/', (req, res) => {
+  console.log(req);
+  res.send('error');
+});
+
+
+
+// exercice 4 = améliorer méthode 
+const authorsAndBooks = {authorsAndBooks:['Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US', 'Oscar Wilde, UK' ]};
+
+app.get('/json/authors/:id/', (req, res) => {
+ 
+      const { name, nationality } = authorsAndBooks[req.params.id - 1]
+      res.json({
+          name,
+          nationality
+      });
+  
+});
+
+app.get('/json/authors/:id/books', (req, res) => {
+ 
+      const { books } = authorsAndBooks[req.params.id - 1]
+      res.json({ books });
+  
+});
+
+
+
+
+// Run server 
 app.listen(port, () => {
   console.log('Server started on port: ' + port);
 });
-
-// exercice 3
 
 
 
